16171365509dc2dcf6d2c5e20ef469320827f7ed
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-12 12:00:19 -0600

    display values from data props to DOM

--- who-fed-the-dog/src/components/ListData.vue
+++ who-fed-the-dog/src/components/ListData.vue
@@ -1,9 +1,16 @@
 <template>
-<div>
-  <p>{{this.data}}}</p>
-  <p>hey</p>
-  <p>hey</p>
-</div>
+  <ul>
+    <li
+      v-for="(value, index) in data[0]"
+      :key="index">
+      <p>{{value.id}}</p>
+      <p>{{value.username}}</p>
+      <p>{{value.petName}}</p>
+      <p>Pet ID: {{value.pet_id}}</p>
+      <p>User ID: {{value.user_id}}</p>
+      <p>{{value.fedOn}}</p>
+      </li>
+  </ul>
 </template>
 
 <script>
@@ -13,4 +20,3 @@ export default {
   }
 }
 </script>
-
