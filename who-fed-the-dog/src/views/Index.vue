50362b95147a7bbba9a629fa14308b6adaa0a3ff
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-14 09:43:18 -0600

    refactor index, comment code out used in other views

--- who-fed-the-dog/src/views/Index.vue
+++ who-fed-the-dog/src/views/Index.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <p>this is the index.vue file in the router-view tag in app.vue I need to destructure this after I get elements wired up correctly.</p>
-    <v-btn @click="getUsers">List all {{ labels[0] }}</v-btn>
-    <v-btn @click="getPets">List all {{ labels[1] }}</v-btn>
-    <v-btn @click="getFedLog">List {{ labels[2] }} Log </v-btn>
+    <!-- <v-btn @click="getUsers">List all {{ labels[0] }}</v-btn> -->
+    <!-- <v-btn @click="getPets">List all {{ labels[1] }}</v-btn> -->
+    <!-- <v-btn @click="getFedLog">List {{ labels[2] }} Log </v-btn> -->
     <v-btn @click="postFed">Mark Pet As Fed</v-btn>
-    <form action="">
+    <!-- <form action="">
       <label for="newUser">Username: </label>
       <v-text-field
         type="text"
@@ -21,8 +21,8 @@
         @click.prevent="findUser"
         type="submit">Find User</v-btn>
       <v-btn>Edit {{ user }}</v-btn>
-    </form>
-     <form action="">
+    </form> -->
+     <!-- <form action="">
       <label for="newPet">Pet Name: </label>
       <v-text-field
         type="text"
@@ -37,7 +37,7 @@
         @click.prevent="findPet"
         type="submit">Find Pet</v-btn>
       <v-btn>Edit {{ pet }}</v-btn>
-    </form>
+    </form> -->
     <list-data :data="toPage"/>
   </v-app>
 </template>
@@ -55,42 +55,42 @@ export default {
     }
   },
   methods: {
-    getUsers() {
-      const apiUrl = `http://localhost:8080/api/v1/routes/${this.labels[0]}`;
-      fetch(apiUrl)
-        .then(Response => Response.json())
-        .then(Response => {
-          this.toPage = [];
-          this.toPage.push(Response.users);
-        })
-    },
-    findUser() {
-      const apiUrl = `http://localhost:8080/api/v1/routes/${this.labels[0]}/${this.user}`;
-      fetch(apiUrl)
-        .then(Response => Response.json())
-        .then(Response => {
-          this.toPage = [];
-          this.toPage.push(Response);
-        })
-    },
-    getPets() {
-      const apiUrl = 'http://localhost:8080/api/v1/routes/pets';
-      fetch(apiUrl)
-        .then(Response => Response.json())
-        .then(Response => {
-          this.toPage = [];
-          this.toPage.push(Response.pets);
-        })
-    },
-    findPet() {
-      const apiUrl = `http://localhost:8080/api/v1/routes/pets/${this.pet}`;
-      fetch(apiUrl)
-        .then(Response => Response.json())
-        .then(Response => {
-          this.toPage = [];
-          this.toPage.push(Response);
-        })
-    },
+    // getUsers() {
+    //   const apiUrl = `http://localhost:8080/api/v1/routes/${this.labels[0]}`;
+    //   fetch(apiUrl)
+    //     .then(Response => Response.json())
+    //     .then(Response => {
+    //       this.toPage = [];
+    //       this.toPage.push(Response.users);
+    //     })
+    // },
+    // findUser() {
+    //   const apiUrl = `http://localhost:8080/api/v1/routes/${this.labels[0]}/${this.user}`;
+    //   fetch(apiUrl)
+    //     .then(Response => Response.json())
+    //     .then(Response => {
+    //       this.toPage = [];
+    //       this.toPage.push(Response);
+    //     })
+    // },
+    // getPets() {
+    //   const apiUrl = 'http://localhost:8080/api/v1/routes/pets';
+    //   fetch(apiUrl)
+    //     .then(Response => Response.json())
+    //     .then(Response => {
+    //       this.toPage = [];
+    //       this.toPage.push(Response.pets);
+    //     })
+    // },
+    // findPet() {
+    //   const apiUrl = `http://localhost:8080/api/v1/routes/pets/${this.pet}`;
+    //   fetch(apiUrl)
+    //     .then(Response => Response.json())
+    //     .then(Response => {
+    //       this.toPage = [];
+    //       this.toPage.push(Response);
+    //     })
+    // },
     getFedLog() {
       const apiUrl = 'http://localhost:8080/api/v1/routes/fedon';
       fetch(apiUrl)
@@ -119,74 +119,74 @@ export default {
           console.log(Response)
           });
     },
-    postNewUser() {
-      const apiUrl = 'http://localhost:8080/api/v1/routes/newuser';
-      const data = {
-        username: this.user
-      };
-      fetch(apiUrl, {
-        body: JSON.stringify(data),
-        headers: new Headers ({
-          'Content-Type': 'application/json',
-          mode: 'no-cors'}),
-        method: 'POST',
-      })
-        .then(Response => Response.json())
-        .then(Response => {
-          console.log(Response)
-          });
-    },
-    postNewPet() {
-      const apiUrl = 'http://localhost:8080/api/v1/routes/newpet';
-      const data = {
-        petName: this.pet
-      };
-      fetch(apiUrl, {
-        body: JSON.stringify(data),
-        headers: new Headers ({
-          'Content-Type': 'application/json',
-          mode: 'no-cors'}),
-        method: 'POST',
-      })
-        .then(Response => Response.json())
-        .then(Response => {
-          console.log(Response)
-          });
-    },
-    deletePet() {
-      const apiUrl = `http://localhost:8080/api/v1/routes/removepet/${this.pet}`;
-      const data = {
-        petName: this.pet
-      };
-      fetch(apiUrl, {
-        body: JSON.stringify(data),
-        headers: new Headers ({
-          'Content-Type': 'application/json',
-          mode: 'no-cors'}),
-        method: 'DELETE',
-      })
-        .then(Response => Response.json())
-        .then(Response => {
-          console.log(Response)
-          });
-    },
-    deleteUser() {
-      const apiUrl = `http://localhost:8080/api/v1/routes/removeuser/${this.user}`;
-      const data = {
-        username: this.user
-      };
-      fetch(apiUrl, {
-        body: JSON.stringify(data),
-        headers: new Headers ({
-          'Content-Type': 'application/json',
-          mode: 'no-cors'}),
-        method: 'DELETE',
-      })
-        .then(Response => Response.json())
-        .then(Response => {
-          console.log(Response)
-          });
-    },
+    // postNewUser() {
+    //   const apiUrl = 'http://localhost:8080/api/v1/routes/newuser';
+    //   const data = {
+    //     username: this.user
+    //   };
+    //   fetch(apiUrl, {
+    //     body: JSON.stringify(data),
+    //     headers: new Headers ({
+    //       'Content-Type': 'application/json',
+    //       mode: 'no-cors'}),
+    //     method: 'POST',
+    //   })
+    //     .then(Response => Response.json())
+    //     .then(Response => {
+    //       console.log(Response)
+    //       });
+    // },
+    // postNewPet() {
+    //   const apiUrl = 'http://localhost:8080/api/v1/routes/newpet';
+    //   const data = {
+    //     petName: this.pet
+    //   };
+    //   fetch(apiUrl, {
+    //     body: JSON.stringify(data),
+    //     headers: new Headers ({
+    //       'Content-Type': 'application/json',
+    //       mode: 'no-cors'}),
+    //     method: 'POST',
+    //   })
+    //     .then(Response => Response.json())
+    //     .then(Response => {
+    //       console.log(Response)
+    //       });
+    // },
+    // deletePet() {
+    //   const apiUrl = `http://localhost:8080/api/v1/routes/removepet/${this.pet}`;
+    //   const data = {
+    //     petName: this.pet
+    //   };
+    //   fetch(apiUrl, {
+    //     body: JSON.stringify(data),
+    //     headers: new Headers ({
+    //       'Content-Type': 'application/json',
+    //       mode: 'no-cors'}),
+    //     method: 'DELETE',
+    //   })
+    //     .then(Response => Response.json())
+    //     .then(Response => {
+    //       console.log(Response)
+    //       });
+    // },
+    // deleteUser() {
+    //   const apiUrl = `http://localhost:8080/api/v1/routes/removeuser/${this.user}`;
+    //   const data = {
+    //     username: this.user
+    //   };
+    //   fetch(apiUrl, {
+    //     body: JSON.stringify(data),
+    //     headers: new Headers ({
+    //       'Content-Type': 'application/json',
+    //       mode: 'no-cors'}),
+    //     method: 'DELETE',
+    //   })
+    //     .then(Response => Response.json())
+    //     .then(Response => {
+    //       console.log(Response)
+    //       });
+    // },
 
   },
   components: {
