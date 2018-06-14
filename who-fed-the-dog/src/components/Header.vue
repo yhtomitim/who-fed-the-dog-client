295cefd2dbe6cd35af1ce0a68ccd26b43e18120d
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-14 09:42:45 -0600

    add nav

--- who-fed-the-dog/src/components/Header.vue
+++ who-fed-the-dog/src/components/Header.vue
@@ -1,7 +1,24 @@
 <template>
-  <v-footer>
-    <p>Who Fed The Dog?!</p>
-  </v-footer>
+  <header>
+    <v-footer>
+      <p>Who Fed The Dog?!</p>
+      <nav>
+      <v-chip>
+        <router-link to="/">Home</router-link>
+      </v-chip>
+      <v-chip>
+        <router-link to="/users">Users</router-link>
+      </v-chip>
+      <v-chip>
+        <router-link to="/pets">Pets</router-link>
+      </v-chip>
+      <v-chip>
+        <router-link to="/log">Feeding Log</router-link>
+      </v-chip>
+    </nav>
+    </v-footer>
+    
+  </header>
 </template>
 
 <script>
