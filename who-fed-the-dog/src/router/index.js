880084519fe4ac09449c90c3e346e402b17c46ac
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-14 09:42:55 -0600

    add import for pets

--- who-fed-the-dog/src/router/index.js
+++ who-fed-the-dog/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/views/Index'
 import Users from '@/views/Users'
+import Pets from '@/views/Pets'
 import Log from '@/views/Log'
 
 Vue.use(Router)
