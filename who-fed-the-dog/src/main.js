a87359fa39c5cc9b6d662dc57fc44d52d0cb53d7
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-15 14:20:54 -0600

    added color theme

--- who-fed-the-dog/src/main.js
+++ who-fed-the-dog/src/main.js
@@ -5,8 +5,16 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.purple.base,
+    secondary: colors.grey.darken1,
+    accent: colors.shades.black,
+    error: colors.red.accent3
+}
+})
 
 Vue.config.productionTip = false
 
