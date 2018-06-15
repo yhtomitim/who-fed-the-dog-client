0f71f0dc00638d35e571e660634bafd6c03b19d9
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-15 13:40:52 -0600

    edit comments

--- who-fed-the-dog/src/LineChart.js
+++ who-fed-the-dog/src/LineChart.js
@@ -11,8 +11,6 @@ export default {
   mixins: [reactiveProp],
   props: ['options'],
   mounted() {
-    // this.chartData is created in the mixin.
-    // If you want to pass options please create a local options object
     this.renderChart(this.chartData, this.options)
   }
 }
