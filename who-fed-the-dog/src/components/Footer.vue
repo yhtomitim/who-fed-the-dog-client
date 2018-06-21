04f0c1902b8af86c8a9ed82ada4045d5bc5ffaa7
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-21 11:32:03 -0600

    add source code links to footer, add layout styling

--- who-fed-the-dog/src/components/Footer.vue
+++ who-fed-the-dog/src/components/Footer.vue
@@ -1,6 +1,21 @@
 <template>
-   <v-footer>
-      <span>&copy; 2018, under MIT license.</span>
+   <v-footer pb-5>
+      <v-flex d-flex>
+        <span class="pl-3">&copy; 2018, under MIT license.</span>
+        <span class="text-md-center">Front-end source code: <a href="https://github.com/yhtomitim/who-fed-the-dog-client">GitHub</a></span>
+        <span class="text-md-right pr-3">Back-end source code: <a href="https://github.com/yhtomitim/who-fed-the-dog-server">GitHub</a></span>
+      </v-flex>
+
+
     </v-footer>
 </template>
+
+<style>
+  a {
+    text-decoration: underline;
+    color: black;
+  }
+
+</style>
+
 
