50932ccb329570204319db1212d9dbd15424e9a5
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-21 11:38:04 -0600

    change footer position to fixed

--- who-fed-the-dog/src/components/Footer.vue
+++ who-fed-the-dog/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-   <v-footer pb-5>
+   <v-footer
+    pb-5
+    fixed>
       <v-flex d-flex>
         <span class="pl-3">&copy; 2018, under MIT license.</span>
         <span class="text-md-center">Front-end source code: <a href="https://github.com/yhtomitim/who-fed-the-dog-client">GitHub</a></span>
@@ -13,7 +15,7 @@
 <style>
   a {
     text-decoration: underline;
-    color: black;
+    color: #000000;
   }
 
 </style>
