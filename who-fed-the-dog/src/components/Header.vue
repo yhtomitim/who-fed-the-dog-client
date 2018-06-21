67f1bd206cab223b9cbd18169fb6d1a8fba733bb
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-21 11:47:14 -0600

    change v-footer to v-toolbar, add position styling

--- who-fed-the-dog/src/components/Header.vue
+++ who-fed-the-dog/src/components/Header.vue
@@ -1,10 +1,11 @@
 <template>
   <header>
+    <v-toolbar
+      fixed
+      class="py-3 mt-1">
     <v-layout
           align-center
-          justify-center
-          pt-5>
-    <v-footer>
+          justify-center>
       <nav>
       <v-btn to="/">Home</v-btn>
       <v-btn to="/users">Users</v-btn>
@@ -12,8 +13,8 @@
       <v-btn to="/log">Pet Feeding Log</v-btn>
        <v-btn to="/chart">App Statistics</v-btn>
     </nav>
-    </v-footer>
     </v-layout>
+    </v-toolbar>
   </header>
 </template>
 
